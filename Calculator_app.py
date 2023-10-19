b0e8693f198e3db8875abf58f94a528a4f6fdd71
SausanCantik <nafisah.sausan@gmail.com>
2023-10-19 13:51:26 +0700

    initial commit

---
+++ Calculator_app.py
@@ -0,0 +1,45 @@
+# app.py
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Import your calculator functions here
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero."
+    return x / y
+
+@app.route('/')
+def home():
+    return render_template('calculator.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+
+    if operation == 'add':
+        result = add(num1, num2)
+    elif operation == 'subtract':
+        result = subtract(num1, num2)
+    elif operation == 'multiply':
+        result = multiply(num1, num2)
+    elif operation == 'divide':
+        result = divide(num1, num2)
+
+    return render_template('calculator.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
